cdbb3a594ddcb4a15cbc43c6b8153f2d3b346601
MrAbyssal <mrabyssal@gmail.com>
2018-03-06 14:11:22 +1030

    Added base box file
    targets Fusion boxstarter scripts for now

---
+++ base1.ps1
@@ -0,0 +1,28 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+# Imports
+. { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/lib__refresh-env.ps1 } | iex;
+
+# Tooling
+. { iwr -useb http://psget.net/GetPsGet.ps1 } | iex
+. { iwr -useb https://chocolatey.org/install.ps1 } | iex;
+. { iwr -useb https://get.scoop.sh } | iex;
+
+choco feature enable -n allowGlobalConfirmation
+choco install Boxstarter
+choco install powershell
+scoop install sudo
+
+refreshenv
+refresh-path
+
+# Dev
+choco install git
+choco install git-credential-manager-for-windows
+choco install openssh
+choco install poshgit
+
+# Languages
+# . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__python.ps1 } | iex;
+. { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__java.ps1 } | iex;
+. { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__nodejs.ps1 } | iex;

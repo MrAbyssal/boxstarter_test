93c654f0fe68bee021c0b9180281d41ae35c198b
MrAbyssal <mrabyssal@gmail.com>
2018-03-07 12:58:17 +1030

    Installs utility programs

---
+++ utilities.ps1
@@ -0,0 +1,15 @@
+set-executionpolicy RemoteSigned
+
+
+choco install f.lux
+#choco install zoomit
+choco install sharex
+choco install bonjour
+choco install 7zip
+choco install vlc
+#choco install enpass.install
+#choco install autohotkey.portable
+choco install shrewsoftvpn
+choco install gimp
+choco install ultravnc
+choco install putty
